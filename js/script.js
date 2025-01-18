e0e3ca0313e9b8fa70ac0808f5956f2cac32042e
Joaquin <joaquin.butera@gmail.com>
2025-01-18 02:56:35 -0300

    feat: funcionalidades del carrito

--- js/script.js
+++ js/script.js
@@ -15,13 +15,36 @@ document.addEventListener("DOMContentLoaded", () => {
     else link.classList.remove("active");
   });
 
+  function toastifyNotification(message, image) {
+    Toastify({
+      text: message,
+      close: false,
+      avatar: image,
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      className: "tostify",
+      style: {
+        background: "#ffffff",
+        color: "#000000",
+        display: "flex",
+        alignItems: "center",
+        gap: "10px",
+        padding: "20px 10px",
+      },
+    }).showToast();
+  }
+
+  function updateCartCounter() {
+    cartLinkCounter.textContent = cart.length;
+  }
+
   async function getData(id = null) {
     const response =
       id === null
         ? await fetch("https://fakestoreapi.com/products?sort=desc")
         : await fetch(`https://fakestoreapi.com/products/${id}`);
     const data = await response.json();
-    console.log(data);
     return data;
   }
 
@@ -29,24 +52,18 @@ document.addEventListener("DOMContentLoaded", () => {
     try {
       const data = await getData();
 
+      // Limpia la sección de productos antes de agregar nuevos
+      productsSection.innerHTML = "";
+
       data.forEach((product) => {
-        console.log(product.id);
-        const buttonText = cart.includes(JSON.parse(product.id))
-          ? "Agregado"
-          : "Agregar al carrito";
-        const buttonClass = cart.includes(product.id)
-          ? "btn-added"
-          : "btn-primary";
+        const isInCart = cart.some((item) => item.id === product.id); // Verifica si el producto está en el carrito
 
-        let buttonIcon = `
-          <i class="fas fa-shopping-cart"></i>
-        `;
+        const buttonText = isInCart ? "Agregado" : "Agregar al carrito";
+        const buttonClass = isInCart ? "btn-added" : "btn-primary";
 
-        if (cart.includes(product.id)) {
-          buttonIcon = `
-            <i class="fa-regular fa-circle-check"></i>
-          `;
-        }
+        let buttonIcon = isInCart
+          ? `<i class="fa-regular fa-circle-check"></i>`
+          : `<i class="fas fa-shopping-cart"></i>`;
 
         productsSection.innerHTML += `
           <article class="product-card" data-id="${product.id}">
@@ -67,15 +84,40 @@ document.addEventListener("DOMContentLoaded", () => {
         `;
       });
 
+      // Agrega event listeners a los botones después de renderizar
       const productButtons = document.querySelectorAll(".product-card__button");
 
       productButtons.forEach((button) => {
         button.addEventListener("click", (e) => {
           const productCard = e.target.closest(".product-card");
           const productId = productCard.getAttribute("data-id");
+          const productTitle = productCard.querySelector(
+            ".product-card__title"
+          ).textContent;
+          const productPrice = productCard
+            .querySelector(".product-card__price")
+            .textContent.replace("$", "");
+          const productImage = productCard.querySelector(
+            ".product-card__image"
+          ).src;
 
-          console.log(`Producto ID: ${productId}`);
-          addProductToCart(productId);
+          // Objeto del producto, así se va a guardar en el localStorage
+          const product = {
+            id: productId,
+            title: productTitle,
+            price: parseFloat(productPrice),
+            image: productImage,
+          };
+
+          addProductToCart(product);
+
+          // Actualiza el botón tras agregar el producto
+          const isInCart = cart.some((item) => item.id === productId);
+          if (isInCart) {
+            button.innerHTML = `<i class="fa-regular fa-circle-check"></i> Agregado`;
+            button.classList.remove("btn-primary");
+            button.classList.add("btn-added");
+          }
         });
       });
     } catch (error) {
@@ -83,51 +125,54 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  function addProductToCart(productId) {
-    if (!cart.includes(productId)) {
-      cart.push(productId);
+  function addProductToCart(product) {
+    console.log(product);
+
+    if (!cart.some((item) => item.id === product.id)) {
+      cart.push(product);
       localStorage.setItem("carrito", JSON.stringify(cart));
-      console.log(`Producto ${productId} agregado al carrito`);
-      Toastify({
-        text: "Producto agregado al carrito con éxito",
-        duration: -1,
-        close: false,
-        avatar: "../assets/img/checkicon.png",
-        gravity: "bottom",
-        position: "right",
-        stopOnFocus: true,
-        style: {
-          background: "#ffffff",
-          color: "#000000",
-          display: "flex",
-          alignItems: "center",
-          gap: "10px",
-          padding: "20px 10px",
-        },
-      }).showToast();
+      const productCard = document.querySelector(
+        `.product-card[data-id="${product.id}"]`
+      );
+      const button = productCard.querySelector(".product-card__button");
+      // Cambia el texto, ícono y clase del botón
+      button.innerHTML = `<i class="fa-regular fa-circle-check"></i> Agregado`;
+      button.classList.remove("btn-primary");
+      button.classList.add("btn-added");
+      console.log(`Producto ${product.title} agregado al carrito.`);
+      toastifyNotification(
+        "¡Producto agregado al carrito con éxito!",
+        "../assets/img/checkicon.png"
+      );
+      updateCartCounter();
     } else {
-      console.log(`Producto ${productId} ya está en el carrito`);
+      console.log(`El producto con el id ${product.id} ya está en el carrito`);
+      toastifyNotification(
+        "¡Este producto ya está en el carrito!",
+        "../assets/img/erroricon.png"
+      );
     }
   }
 
   function getProductsFromCart() {
     console.log(cart);
-    row.innerHTML = "";
+    row.innerHTML = ""; // Limpia la tabla antes de renderizar
 
+    // Si el carrito está vacío, muestra el mensaje
     if (cart.length === 0) {
       row.innerHTML = `
         <tr>
-          <td colspan="3" class="empty-cart-message">
-            No hay productos en el carrito.
+          <td colspan="5" class="empty-cart-message">
+            No hay productos en el carrito
           </td>
         </tr>`;
       return;
     }
 
-    cart.forEach(async (productId) => {
-      const product = await getData(productId);
-      console.log(product);
-      row.innerHTML += `<tr>
+    // Itera sobre los productos en el carrito
+    cart.forEach((product) => {
+      row.innerHTML += `
+        <tr>
           <td class="w-33">${product.title}</td>
           <td class="w-33">
             <img
@@ -137,8 +182,12 @@ document.addEventListener("DOMContentLoaded", () => {
             />
           </td> 
           <td class="w-33">$${product.price}</td>
-          <td class="w-33"><input type="number" min="1" max="20" value="1"/></td>
-          <td class="w-33"><i class="fa-solid fa-trash-can trash-icon" data-id="${product.id}"></i></td>
+          <td class="w-33">
+            <input type="number" min="1" max="20" value="1" class="product-quantity" data-id="${product.id}" />
+          </td>
+          <td class="w-33">
+            <i class="fa-solid fa-trash-can trash-icon" data-id="${product.id}"></i>
+          </td>
         </tr>`;
     });
   }
@@ -150,14 +199,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (response) {
       // Se filtra el producto a eliminar. Devuelve un nuevo arreglo con los elementos distintos al id del producto a eliminar
-      cart = cart.filter((id) => id !== productId);
+      cart = cart.filter((product) => product.id !== productId);
 
-      // "cart" es un nuevo arreglo sin ese id y se guarda este nuevo arreglo en localstorage. De esta manera
+      // "cart" es un nuevo arreglo sin ese id y se guarda este nuevo arreglo en localstorage.
       localStorage.setItem("carrito", JSON.stringify(cart));
       console.log(`Producto ${productId} eliminado del carrito.`);
+      toastifyNotification(
+        "¡Producto eliminado del carrito con éxito!",
+        "../assets/img/checkicon.png"
+      );
 
       // Se reutiliza la función para volver a imprimir los productos actualizados
       getProductsFromCart();
+      updateCartCounter();
     }
   }
 
